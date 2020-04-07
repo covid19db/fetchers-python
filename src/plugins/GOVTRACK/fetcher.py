e6a3dabc7c51c24265066cb03f77ef8b60b49f79
Piotr Blaszczyk <piotr.blaszczyk82@gmail.com>
2020-04-07 20:33:07 +0100

    Add GOVTRACK actions

--- src/plugins/GOVTRACK/fetcher.py
+++ src/plugins/GOVTRACK/fetcher.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import requests
 from .utils import parser
@@ -19,9 +20,17 @@ class StringencyFetcher(AbstractFetcher):
             f'https://covidtrackerapi.bsg.ox.ac.uk/api/stringency/date-range/{date_from}/{date_to}').json()
         return parser(api_data)
 
+    def fetch_details(self, country_code, date_value):
+        api_details = requests.get(
+            f'https://covidtrackerapi.bsg.ox.ac.uk/api/stringency/actions/{country_code}/{date_value}'
+        ).json()
+        api_details.pop("stringencyData")
+        return api_details
+
     def run(self):
         govtrack_data = self.fetch()
         for index, record in govtrack_data.iterrows():
+            govtrack_actions = self.fetch_details(record['country_code'], record['date_value'])
             upsert_obj = {
                 'source': 'GOVTRACK',
                 'date': record['date_value'],
@@ -30,6 +39,7 @@ class StringencyFetcher(AbstractFetcher):
                 'confirmed': int(record['confirmed']),
                 'dead': int(record['deaths']),
                 'stringency': int(record['stringency']),
-                'stringency_actual': int(record['stringency_actual'])
+                'stringency_actual': int(record['stringency_actual']),
+                'actions': json.dumps(govtrack_actions)
             }
             self.db.upsert_govtrack_data(**upsert_obj)
