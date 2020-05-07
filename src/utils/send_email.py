21d9604fbd1e2804c99ad965c9c62e35f7c85555
bitsbytenibbles <61532566+bitsbytenibbles@users.noreply.github.com>
2020-05-07 22:47:54 +0400

    update docker compose and send_email

--- src/utils/send_email.py
+++ src/utils/send_email.py
@@ -1,16 +1,19 @@
 import smtplib 
 from utils.adapters import DataAdapter
+import os
 
 #function sends an email to the contact related to the fetcher 
 
 def send_email(source: str, message: str):
+        email = os.getenv('SYS_EMAIL')
+        password = os.getenv('SYS_EMAIL_PASS')
 
         db = DataAdapter.get_adapter()
         destination=db.select_email(source)
         
         s = smtplib.SMTP('smtp.gmail.com', 587) 
         s.starttls() 
-        s.login("ahmadandreem@gmail.com", "adamluna2009") 
+        s.login(email, password) 
         s.sendmail("covid19server", destination, message) 
         s.quit()
 
