05bb1f1f569656799aad8a30d1d7fe4208f4dac4
Piotr Blaszczyk <piotr.blaszczyk82@gmail.com>
2020-05-17 11:21:51 +0100

    Update Google Mobility fetcher to use hierarchical translation

--- src/plugins/GOOGLE_MOBILITY/fetcher.py
+++ src/plugins/GOOGLE_MOBILITY/fetcher.py
@@ -1,8 +1,9 @@
+import os
+import csv
 import logging
 import pandas as pd
 from utils.fetcher_abstract import AbstractFetcher
-
-from .utils import get_country_codes
+from .utils import get_country_codes, get_country_info
 
 __all__ = ('GoogleMobilityFetcher',)
 
@@ -10,49 +11,75 @@ logger = logging.getLogger(__name__)
 
 
 class GoogleMobilityFetcher(AbstractFetcher):
-    LOAD_PLUGIN = False
+    LOAD_PLUGIN = True
 
     def fetch(self):
         # Google covid19 mobility data
         url = 'https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv'
-        return pd.read_csv(url)
+        return pd.read_csv(url, low_memory=False)
+
+    def get_region(self, countrycode, input_adm_area_1, input_adm_area_2, input_adm_area_3):
+        try:
+            # Check if input data can be matched directly into administrative division table
+            adm_area_1, adm_area_2, adm_area_3, gid = self.db.get_adm_division(
+                countrycode, input_adm_area_1, input_adm_area_2, input_adm_area_3)
+        except Exception as ex:
+            adm_area_1, adm_area_2, adm_area_3, gid = None, None, None, None
+
+        if not gid:
+            # Check translate.csv for translation
+            success, adm_area_1, adm_area_2, adm_area_3, gid = self.adm_translator.tr(
+                input_adm_area_1, input_adm_area_2, input_adm_area_3)
+
+        return adm_area_1, adm_area_2, adm_area_3, gid
 
     def run(self):
         data = self.fetch()
         country_codes = get_country_codes()
 
+        unknown_regions = set()
+        region_cache = {}
+
         for index, record in data.iterrows():
             date = record['date']
             country_a2_code = record['country_region_code']
 
-            country_info = country_codes[country_codes['Alpha-2 code'] == country_a2_code].to_dict('records')[0]
-            countrycode = country_info["Alpha-3 code"]
-            country = country_info["English short name lower case"]
+            country, countrycode = get_country_info(country_codes, country_a2_code)
 
-            input_adm_area_1 = record['sub_region_1'] if pd.notna(record['sub_region_1']) else None
-            input_adm_area_2 = record['sub_region_2'] if pd.notna(record['sub_region_2']) else None
+            input_adm_area_1 = record['sub_region_1'].strip() if pd.notna(record['sub_region_1']) else None
+            input_adm_area_2 = record['sub_region_2'].strip() if pd.notna(record['sub_region_2']) else None
 
-            try:
-                # Check if input data can be matched directly into administrative division table
-                gid = self.db.get_gid(countrycode, input_adm_area_1, input_adm_area_2)
-                adm_area_1 = input_adm_area_1
-                adm_area_2 = input_adm_area_2
-            except Exception as ex:
-                gid = None
+            if countrycode == 'USA':
+                if input_adm_area_2:
+                    input_adm_area_2 = input_adm_area_2.replace('County', '') \
+                        .replace('Parish', '') \
+                        .replace('St.', 'Saint').strip()
+            elif countrycode == 'GBR' and input_adm_area_1:
+                # Use adm_area_2 for Great Britain
+                input_adm_area_2 = input_adm_area_1
+                input_adm_area_1 = '%'
+            elif input_adm_area_1:
+                input_adm_area_1 = input_adm_area_1.replace('Province', '') \
+                    .replace('District', '').replace('County', '') \
+                    .replace('Region', '').replace('Governorate', '') \
+                    .replace('State of', '').strip()
 
+            key = (countrycode, input_adm_area_1, input_adm_area_2, '')
+            if key in region_cache:
+                adm_area_1, adm_area_2, adm_area_3, gid = region_cache.get(key)
+            else:
+                adm_area_1, adm_area_2, adm_area_3, gid = self.get_region(
+                    countrycode, input_adm_area_1, input_adm_area_2, None)
+                region_cache[key] = (adm_area_1, adm_area_2, adm_area_3, gid)
             if not gid:
-                # check in translate.csv for translation
-                success, adm_area_1, adm_area_2, adm_area_3, gid = self.adm_translator.tr(
-                    input_adm_area_1=input_adm_area_1,
-                    input_adm_area_2=input_adm_area_2
-                )
-
-            if not gid:
-                raise Exception(
+                # raise Exception(
+                #    f'Unable to find translation for: "{countrycode}", "{input_adm_area_1}", "{input_adm_area_2}" '
+                #    f'add correct translation in CSV file')
+                logger.warning(
                     f'Unable to find translation for: "{countrycode}", "{input_adm_area_1}", "{input_adm_area_2}" '
                     f'add correct translation in CSV file')
+                unknown_regions.add(key)
 
-            # Upsert to mobility
             upsert_obj = {
                 'source': 'GOOGLE_MOBILITY',
                 'date': date,
@@ -69,4 +96,12 @@ class GoogleMobilityFetcher(AbstractFetcher):
                 'grocery_pharmacy': record['residential_percent_change_from_baseline']
             }
 
-            self.db.upsert_mobility_data(**upsert_obj)
+            if gid:
+                self.db.upsert_mobility_data(**upsert_obj)
+
+        logger.warning('Unknown regions total: {}'.format(len(unknown_regions)))
+        unknown_regions_list = sorted(list(unknown_regions), key=lambda x: (x[0] or '', x[1] or '', x[2] or ''))
+        with open(os.path.join(os.path.dirname(__file__), 'unknown_regions.csv'), 'w') as unknown_regions_file:
+            csv_writer = csv.writer(unknown_regions_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            for region in unknown_regions_list:
+                csv_writer.writerow(list(region))
