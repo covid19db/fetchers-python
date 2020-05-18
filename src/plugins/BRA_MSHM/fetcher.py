471b648f972e2031762ea236378d2efe72e8119c
Piotr Blaszczyk <piotr.blaszczyk82@gmail.com>
2020-05-18 21:35:06 +0100

    Code fixes in fetchers

--- src/plugins/BRA_MSHM/fetcher.py
+++ src/plugins/BRA_MSHM/fetcher.py
@@ -48,60 +48,55 @@ class BRA_MSHMFetcher(AbstractFetcher):
                         province_confirmed_fetch and province_dead_fetch;
         
         """
-        
+
         province_confirmed_data = self.province_confirmed_fetch()
         province_dead_data = self.province_dead_fetch()
-        
+
         ### Get province names list
         province_list = list(province_confirmed_data["Unnamed: 0"])[1:]
 
         ### Get dates list
         time_list = list(province_confirmed_data.columns)[2:]
-        
+
         ### Get country_level confirmed and dead case
-        
-        country_confirmed_array=np.array(province_confirmed_data.iloc[0,2:])
-        country_dead_array=np.array(province_dead_data.iloc[0,2:])
-        
+
+        country_confirmed_array = np.array(province_confirmed_data.iloc[0, 2:])
+        country_dead_array = np.array(province_dead_data.iloc[0, 2:])
+
         for j in range(len(time_list)):
-            
             ### Translating data format from DD/MM to YYYY-MM-DD
             date_ddmm = time_list[j]
             date = datetime.strptime(date_ddmm + "/2020", '%d/%m/%Y').strftime('%Y-%m-%d')
-            
-            confirmed=country_confirmed_array[j]
-            dead=country_dead_array[j]
-            
-            upsert_obj = {
-                    # source is mandatory and is a code that identifies the  source
-                    'source': 'BRA_MSHM',
-                    # date is also mandatory, the format must be YYYY-MM-DD
-                    'date': date,
-                    # country is mandatory and should be in English
-                    # the exception is "Ships"
-                    'country': "Brazil",
-                    # countrycode is mandatory and it's the ISO Alpha-3 code of the country
-                    # an exception is ships, which has "---" as country code
-                    'countrycode': 'BRA',
-                    # adm_area_1, when available, is a wide-area administrative region, like a
-                    # Canadian province in this case. There are also subareas adm_area_2 and
-                    # adm_area_3
-                    'gid'=['BRA'],
-                    'adm_area_1': None,
-                    'adm_area_2': None,
-                    'adm_area_3': None,
-                    'confirmed': int(confirmed),
-                    # dead is the number of people who have died because of covid19, this is cumulative
-                    'dead': int(dead)
 
-                }
+            confirmed = country_confirmed_array[j]
+            dead = country_dead_array[j]
+
+            upsert_obj = {
+                # source is mandatory and is a code that identifies the  source
+                'source': 'BRA_MSHM',
+                # date is also mandatory, the format must be YYYY-MM-DD
+                'date': date,
+                # country is mandatory and should be in English
+                # the exception is "Ships"
+                'country': "Brazil",
+                # countrycode is mandatory and it's the ISO Alpha-3 code of the country
+                # an exception is ships, which has "---" as country code
+                'countrycode': 'BRA',
+                # adm_area_1, when available, is a wide-area administrative region, like a
+                # Canadian province in this case. There are also subareas adm_area_2 and
+                # adm_area_3
+                'gid': ['BRA'],
+                'adm_area_1': None,
+                'adm_area_2': None,
+                'adm_area_3': None,
+                'confirmed': int(confirmed),
+                # dead is the number of people who have died because of covid19, this is cumulative
+                'dead': int(dead)
+
+            }
 
             self.db.upsert_epidemiology_data(**upsert_obj)
 
-
-            
-            
-        
         ### For province-level
         for k in range(len(time_list)):
 
@@ -118,8 +113,8 @@ class BRA_MSHMFetcher(AbstractFetcher):
                 province = province_list[i]
                 confirmed = current_confirm_list[1 + i]
                 dead = current_dead_list[1 + i]
-                
-                'adm_area_1', 'adm_area_2', 'adm_area_3', 'gid' = self.db.get_adm_division('BRA', province, None, None)
+
+                adm_area_1, adm_area_2, adm_area_3, gid = self.db.get_adm_division('BRA', province)
                 upsert_obj = {
                     # source is mandatory and is a code that identifies the  source
                     'source': 'BRA_MSHM',
@@ -134,9 +129,10 @@ class BRA_MSHMFetcher(AbstractFetcher):
                     # adm_area_1, when available, is a wide-area administrative region, like a
                     # Canadian province in this case. There are also subareas adm_area_2 and
                     # adm_area_3
-                    'adm_area_1': province,
+                    'adm_area_1': adm_area_1,
                     'adm_area_2': None,
                     'adm_area_3': None,
+                    'gid': gid,
                     'confirmed': int(confirmed),
                     # dead is the number of people who have died because of covid19, this is cumulative
                     'dead': int(dead)
@@ -144,4 +140,3 @@ class BRA_MSHMFetcher(AbstractFetcher):
                 }
 
                 self.db.upsert_epidemiology_data(**upsert_obj)
-
