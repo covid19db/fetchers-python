f4bfb4ec935cb99ddf761ee0a8123c497335ea25
Piotr Blaszczyk <piotr.blaszczyk82@gmail.com>
2020-05-25 18:14:14 +0100

    Fix for adapter wrapper, update logger format

--- src/utils/adapter_wrapper.py
+++ src/utils/adapter_wrapper.py
@@ -34,21 +34,21 @@ class AdapterWrapper(AbstractAdapter):
     def upsert_government_response_data(self, table_name: str = 'government_response', **kwargs):
         if not self.date_in_window(kwargs):
             return
-        self.data_adapter.upsert_government_response_data(self.correct_table_name(table_name), **kwargs)
+        return self.data_adapter.upsert_government_response_data(self.correct_table_name(table_name), **kwargs)
 
     def upsert_epidemiology_data(self, table_name: str = 'epidemiology', **kwargs):
         if not self.date_in_window(kwargs):
             return
-        self.data_adapter.upsert_epidemiology_data(self.correct_table_name(table_name), **kwargs)
+        return self.data_adapter.upsert_epidemiology_data(self.correct_table_name(table_name), **kwargs)
 
     def upsert_mobility_data(self, table_name: str = 'mobility', **kwargs):
         if not self.date_in_window(kwargs):
             return
-        self.data_adapter.upsert_mobility_data(self.correct_table_name(table_name), **kwargs)
+        return self.data_adapter.upsert_mobility_data(self.correct_table_name(table_name), **kwargs)
 
     def get_adm_division(self, countrycode: str, adm_area_1: str = None, adm_area_2: str = None,
                          adm_area_3: str = None):
-        self.data_adapter.get_adm_division(countrycode, adm_area_1, adm_area_2, adm_area_3)
+        return self.data_adapter.get_adm_division(countrycode, adm_area_1, adm_area_2, adm_area_3)
 
     def flush(self):
         if hasattr(self.data_adapter, 'flush'):
