f2cd5b3565e682bf70df9c1a52cd48dd943cb267
Piotr Blaszczyk <piotr.blaszczyk82@gmail.com>
2020-06-02 17:12:29 +0100

    Remove country column from SQL query

--- src/adapters/postgresql.py
+++ src/adapters/postgresql.py
@@ -147,7 +147,7 @@ class PostgresqlHelper(AbstractAdapter):
         self.check_if_gid_exists(kwargs)
         sql_query = sql.SQL("""INSERT INTO {table_name} ({insert_keys}) VALUES ({insert_data})
                                 ON CONFLICT
-                                    (date, country, countrycode, COALESCE(gid, ''))
+                                    (date, countrycode, COALESCE(gid, ''))
                                 DO
                                     UPDATE SET {update_data}
                                RETURNING *
