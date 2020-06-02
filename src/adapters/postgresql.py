55ffba7c978763b204752f49ee877b4f6aed59f6
Piotr Blaszczyk <piotr.blaszczyk82@gmail.com>
2020-06-02 17:28:54 +0100

    Alter SQL query

--- src/adapters/postgresql.py
+++ src/adapters/postgresql.py
@@ -147,7 +147,7 @@ class PostgresqlHelper(AbstractAdapter):
         self.check_if_gid_exists(kwargs)
         sql_query = sql.SQL("""INSERT INTO {table_name} ({insert_keys}) VALUES ({insert_data})
                                 ON CONFLICT
-                                    (date, countrycode, COALESCE(gid, ''))
+                                    (date, countrycode, gid)
                                 DO
                                     UPDATE SET {update_data}
                                RETURNING *
