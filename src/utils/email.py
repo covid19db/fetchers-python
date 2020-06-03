05d84a708f2291f4ea52591b5e8be0882f149914
Piotr Blaszczyk <piotr.blaszczyk82@gmail.com>
2020-06-03 23:00:15 +0100

    Add subject to email notification

--- src/utils/email.py
+++ src/utils/email.py
@@ -1,12 +1,16 @@
 import re
 import os
+import logging
 import smtplib
 import itertools
 import pandas as pd
+from email.message import EmailMessage
 
 from utils.config import config
 
 EMAIL_REGEX = re.compile(r"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$")
+
+logger = logging.getLogger(__name__)
 
 
 def validate_address(email: str) -> bool:
@@ -16,20 +20,26 @@ def validate_address(email: str) -> bool:
 def send_email(src_code: str, message: str):
     emails_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "emails.csv")
 
-    if os.path.isfile(emails_file_path):
-        df = pd.read_csv(emails_file_path, encoding='utf-8')
+    if not os.path.isfile(emails_file_path):
+        logger.error(f"Unable to send notification email, file: {emails_file_path} doesn't exist")
+        return
 
-        df_recipients = df[(df.source_code == src_code) | (df.source_code == '*')]
-        emails_list = df_recipients[
-            ['email_curator_1', 'email_curator_2', 'email_curator_3', 'email_curator_4']
-        ].values.tolist()
-        emails = set(itertools.chain.from_iterable(emails_list))
-        destination = [email for email in emails if validate_address(email)]
+    df = pd.read_csv(emails_file_path, encoding='utf-8')
+    df_recipients = df[(df.source_code == src_code) | (df.source_code == '*')]
+    emails_list = df_recipients[
+        ['email_curator_1', 'email_curator_2', 'email_curator_3', 'email_curator_4']
+    ].values.tolist()
+    emails = set(itertools.chain.from_iterable(emails_list))
+    receivers = [email for email in emails if validate_address(email)]
 
-        email = config.SYS_EMAIL
-        password = config.SYS_EMAIL_PASS
-        s = smtplib.SMTP('smtp.gmail.com', 587)
+    msg = EmailMessage()
+    msg['Subject'] = f'Covid19db fetchers-python validation error'
+    msg['From'] = "covid19db.org"
+    msg['To'] = receivers
+    msg.set_content(message)
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as s:
         s.starttls()
-        s.login(email, password)
-        s.sendmail("covid19db", destination, message)
-        s.quit()
+        s.login(config.SYS_EMAIL, config.SYS_EMAIL_PASS)
+        s.send_message(msg)
+        logger.info(f'Email successfully sent to: {receivers}')
