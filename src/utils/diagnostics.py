1ca40f31b5fa3ae476f42d39171fa2e39308715f
johnharveymath <60899713+johnharveymath@users.noreply.github.com>
2020-06-30 18:29:36 +0100

    Quick fix to skip diagnostics if writing to CSV, as this causes exception (#81)

--- src/utils/diagnostics.py
+++ src/utils/diagnostics.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from utils.fetcher.abstract_fetcher import AbstractFetcher
+from utils.config import config
 
 __all__ = ('Diagnostics',)
 
@@ -10,15 +11,18 @@ class Diagnostics:
         self.fetcher_instance = fetcher_instance
 
     def update_diagnostics_info(self, validation: bool, error: bool, start_time, end_time):
-        data = {
-            "table_name": self.fetcher_instance.TYPE.value,
-            "source": self.fetcher_instance.SOURCE,
-            "validation_success": validation,
-            "error": error,
-            "last_run_start": datetime.fromtimestamp(start_time),
-            "last_run_stop": datetime.fromtimestamp(end_time),
-            "first_timestamp": self.fetcher_instance.get_earliest_timestamp(),
-            "last_timestamp": self.fetcher_instance.get_latest_timestamp()
-        }
-        data_adapter = self.fetcher_instance.data_adapter
-        data_adapter.upsert_diagnostics(**data)
+        if config.CSV:
+            pass
+        else:
+            data = {
+                "table_name": self.fetcher_instance.TYPE.value,
+                "source": self.fetcher_instance.SOURCE,
+                "validation_success": validation,
+                "error": error,
+                "last_run_start": datetime.fromtimestamp(start_time),
+                "last_run_stop": datetime.fromtimestamp(end_time),
+                "first_timestamp": self.fetcher_instance.get_earliest_timestamp(),
+                "last_timestamp": self.fetcher_instance.get_latest_timestamp()
+            }
+            data_adapter = self.fetcher_instance.data_adapter
+            data_adapter.upsert_diagnostics(**data)
