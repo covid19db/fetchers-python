faa4272514a89f7956ec3030b15743454a6edb03
johnharveymath <60899713+johnharveymath@users.noreply.github.com>
2020-09-16 19:17:51 +0100

    Redesign GBR_NIDH for Excel (#120)

--- src/plugins/GBR_NIDH/fetcher.py
+++ src/plugins/GBR_NIDH/fetcher.py
@@ -1,4 +1,4 @@
-    # Copyright (C) 2020 University of Oxford
+# Copyright (C) 2020 University of Oxford
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,16 +14,8 @@
 
 import logging
 import pandas as pd
-import time
-import os
-import sys
-import csv
 
 from datetime import datetime, date, timedelta
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 __all__ = ('NorthernIrelandFetcher',)
 
@@ -36,179 +28,90 @@ class NorthernIrelandFetcher(BaseEpidemiologyFetcher):
     ''' a fetcher to collect data from Northern Ireland Department of Health'''
     LOAD_PLUGIN = True
     SOURCE = 'GBR_NIDH'  # Northern Ireland Department of Health
-    wd = None
-    date = date.today() - timedelta(hours=6)
 
-    def parse_int(self, myString):
-        if isinstance(myString, int):
-            return myString
-        else:
-            return int(myString.replace(',', ''))
+    def excelreader(self, url):
+        # read the excel file located at url
 
-    def check_aria_label(self, item, phrase):
-        label = item.get_attribute('aria-label')
-        return label.startswith(phrase) if label else False
+        testing_data = pd.read_excel(url, sheet_name='Tests', parse_dates=[0],
+                                     names=['Date', 'LGD', 'Tests', 'Individuals', 'Positives'])
+        testing_data['LGD'].fillna('Unknown', inplace=True)
+        testing_data.replace('Missing Postcode', 'Unknown', inplace=True)
 
-    def lau_label(self, label):
-        lau = label.split(".")[0]
-        prefix = 'Local Government District '
-        return lau[len(prefix):]
+        deaths_data = pd.read_excel(url, sheet_name='Deaths', parse_dates=[0],
+                                    names=['Date', 'LGD', 'Setting', 'Gender', 'Age Band', 'Number of Deaths'])
+        deaths_data['LGD'].fillna('Unknown', inplace=True)
+        deaths_data.replace('Missing Postcode', 'Unknown', inplace=True)
 
-    def lau_deaths(self, label):
-        deaths = label.split()[-1]
-        return self.parse_int(deaths[:-1])
+        return testing_data, deaths_data
 
-    def wd_config(self):
-        # configue a webdriver for selenium
-        # this should probably be set at AbstractFetcher level
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        self.wd = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
-        self.wd.implicitly_wait(10)
+    def fetch(self):
 
-    def bridging_data(self):
-        input_csv_fname = 'bridging_data.csv'
-        path = os.path.dirname(sys.modules[self.__class__.__module__].__file__)
-        csv_fname = os.path.join(path, input_csv_fname)
-        if not os.path.exists(csv_fname):
-            return None
+        # Check the last four days and attempt to read a file with matching name
+        # The name is, sadly, not always the same. Check four variants for each date.
+        datetimeobj = datetime.today()
+        attempts = 4
+        while attempts > 0:
+            try:
+                day = datetimeobj.strftime('%d%m%y')
+                url = f'https://www.health-ni.gov.uk/sites/default/files/publications/health/doh-dd-{day}.xlsx'
+                testing_data, deaths_data = self.excelreader(url)
+                return testing_data, deaths_data
+            except:
+                try:
+                    url = f'https://www.health-ni.gov.uk/sites/default/files/publications/health/doh-dd-{day}.XLSX'
+                    testing_data, deaths_data = self.excelreader(url)
+                    return testing_data, deaths_data
+                except:
+                    try:
+                        url = f'https://www.health-ni.gov.uk/sites/default/files/publications/health/dd-{day}.XLSX'
+                        testing_data, deaths_data = self.excelreader(url)
+                        return testing_data, deaths_data
+                    except:
+                        try:
+                            url = f'https://www.health-ni.gov.uk/sites/default/files/publications/health/dd-{day}.xlsx'
+                            testing_data, deaths_data = self.excelreader(url)
+                            return testing_data, deaths_data
+                        except:
+                            datetimeobj = datetimeobj - timedelta(days=1)
+                            attempts = attempts - 1
 
-        with open(csv_fname, newline='') as csvfile:
-            csv_reader = csv.DictReader(csvfile)
-            for upsert_obj in csv_reader:
-                # translate empty string to None in adm_area_2
-                input_adm_area_2 = upsert_obj.get('adm_area_2')
-                if not input_adm_area_2:
-                    input_adm_area_2 = None
+    def run(self):
 
-                success, adm_area_1, adm_area_2, adm_area_3, gid = self.adm_translator.tr(
-                    input_adm_area_1='Northern Ireland',
-                    input_adm_area_2=input_adm_area_2,
-                    input_adm_area_3=None,
-                    return_original_if_failure=True,
-                    suppress_exception=True
-                )
-                upsert_obj['date'] = datetime.strptime(upsert_obj.get('date'),'%d/%m/%Y').strftime('%Y-%m-%d')
-                upsert_obj['adm_area_2'] = adm_area_2
-                upsert_obj['gid'] = gid
+        # fetch data from the online excel file
+        testing_data, deaths_data = self.fetch()
 
-                self.upsert_data(**upsert_obj)
+        # now group the deaths data to remove detailed info and collapse different 'Unknowns'
+        grouped = deaths_data.groupby(['Date', 'LGD'], as_index=False)
+        deaths_data = grouped[['Number of Deaths']].sum()
 
-                # do the upsert again for level 3
-                if adm_area_2 and adm_area_2 != 'Unknown':
-                    success, adm_area_1, adm_area_2, adm_area_3, gid = self.adm_translator.tr(
-                        input_adm_area_1='Northern Ireland',
-                        input_adm_area_2=input_adm_area_2,
-                        input_adm_area_3=input_adm_area_2,
-                        return_original_if_failure=True,
-                        suppress_exception=True
-                    )
-                    upsert_obj['adm_area_3'] = adm_area_3
-                    upsert_obj['gid'] = gid
-                    self.upsert_data(**upsert_obj)
+        # now group the testing data to collapse different 'Unknowns'
+        grouped = testing_data.groupby(['Date', 'LGD'], as_index=False)
+        testing_data = grouped[['Individuals', 'Positives']].sum()
 
-    def fetch_national(self):
+        # combine the two dataframes
+        df = pd.merge(deaths_data, testing_data, how='outer', left_on=['Date', 'LGD'], right_on=['Date', 'LGD'])
+        df.sort_values(['Date'], inplace=True)
 
-        # go to the national summary page
-        WebDriverWait(self.wd, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//*[name()='a' and @class='middleText']"))
-        ).click()
-        time.sleep(3)
-        WebDriverWait(self.wd, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//*[name()='a' and @title='COVID-19 Summary']"))
-        ).click()
+        # pad out the dataframe with missing dates, then set cumulative sum
+        idx = pd.MultiIndex.from_product([pd.date_range(df.iat[0, 0], df.iat[len(df) - 1, 0]),
+                                          df.LGD.unique()], names=['Date', 'LGD'])
+        df = df.set_index(['Date', 'LGD']).reindex(idx).fillna(0)
+        df = pd.concat([df, df.groupby(level=1).cumsum().add_prefix('Cum_')], 1).sort_index(level=1).reset_index()
 
-        # seems to need a fixed wait period before looking for the headline items
-        time.sleep(5)
-        items = self.wd.find_elements_by_xpath('//*[name()="visual-container-group"]//*[name()="svg"]')
+        # upsert the data
+        for index, record in df.iterrows():
 
-        # pick out items by aria-label
-        total_tested = [self.parse_int(item.text) for item in items if
-                        self.check_aria_label(item, 'Individuals Tested')]
-        confirmed = [self.parse_int(item.text) for item in items if
-                     self.check_aria_label(item, 'Individuals with a Positive Test')]
-        deaths = [self.parse_int(item.text) for item in items if self.check_aria_label(item, 'Num_Deaths')]
-
-        upsert_obj = {
-            'source': self.SOURCE,
-            'date': self.date.strftime('%Y-%m-%d'),
-            'country': 'United Kingdom',
-            'countrycode': 'GBR',
-            'adm_area_1': 'Northern Ireland',
-            'adm_area_2': None,
-            'adm_area_3': None,
-            'gid': ['GBR.2_1'],
-            'confirmed': confirmed[0],
-            'dead': deaths[0],
-            'tested': total_tested[0]
-        }
-
-        self.upsert_data(**upsert_obj)
-
-    def fetch_regional(self):
-
-        # go to the page giving tests by local government
-        WebDriverWait(self.wd, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//*[name()='a' and @class='middleText']"))
-        ).click()
-        time.sleep(3)
-        WebDriverWait(self.wd, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//*[name()='a' and @title='COVID-19 Testing LGD']"))
-        ).click()
-
-        local_govt_table = self.wd.find_element_by_xpath(
-            '//*[name()="div" and @aria-label="Breakdown of Individuals with a Laboratory Completed Test (Pillar 1 & '
-            '2) by Local Government District Table"]//*[name()="div" and @class="bodyCells"]/div/div')
-        columns = local_govt_table.find_elements_by_xpath('./div')
-
-        local_testing_data = []
-        for column in columns:
-            column_data = []
-            for element in column.find_elements_by_xpath('./div'):
-                column_data.append(element.text)
-            local_testing_data.append(column_data)
-
-        data = list(zip(local_testing_data[0], map(self.parse_int, local_testing_data[1]),
-                        map(self.parse_int, local_testing_data[2])))
-        df_test = pd.DataFrame(data, columns=['lau', 'tests', 'positive'])
-
-        # go to the page with deaths by LGD
-        WebDriverWait(self.wd, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//*[name()='a' and @class='middleText']"))
-        ).click()
-        time.sleep(3)
-        WebDriverWait(self.wd, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//*[name()='a' and @title='COVID-19 Death Details']"))
-        ).click()
-
-        time.sleep(5)
-        chart_area = self.wd.find_element_by_xpath(
-            '//div[@aria-label="COVID-19 Deaths by Local Government District Clustered column chart"]')
-        rectangles = chart_area.find_elements_by_xpath(
-            './/*[name()="svg"]/*[name()="svg"]//*[@class="series"]'
-            '//*[name()="rect" and @class="column setFocusRing"]')
-        labels = [rectangle.get_attribute('aria-label') for rectangle in rectangles]
-        lau = [self.lau_label(label) for label in labels]
-        lau_deaths = [self.lau_deaths(label) for label in labels]
-        data = list(zip(lau, lau_deaths))
-        df_deaths = pd.DataFrame(data, columns=['lau', 'deaths'])
-
-        all_local_data = df_test.merge(df_deaths, on='lau')
-
-        for index, record in all_local_data.iterrows():
-            confirmed = record['positive']
-            tested = record['tests']
-            deaths = record['deaths']
-            lgd = record['lau']
-            date = self.date
+            date = record['Date']
+            lgd = record['LGD']
+            confirmed = record['Cum_Positives']
+            tested = record['Cum_Individuals']
+            deaths = record['Cum_Number of Deaths']
 
             success, adm_area_1, adm_area_2, adm_area_3, gid = self.adm_translator.tr(
-                input_adm_area_1 = 'Northern Ireland',
-                input_adm_area_2 = lgd,
-                input_adm_area_3 = None,
-                return_original_if_failure = True,
+                input_adm_area_1='Northern Ireland',
+                input_adm_area_2=lgd,
+                input_adm_area_3=None,
+                return_original_if_failure=True,
                 suppress_exception=True
             )
 
@@ -242,41 +145,37 @@ class NorthernIrelandFetcher(BaseEpidemiologyFetcher):
                 upsert_obj['gid'] = gid
                 self.upsert_data(**upsert_obj)
 
-    def run(self):
+        # now collect info for all Northern Ireland
+        grouped = df.groupby(['Date'], as_index=False)
+        df = grouped[['Cum_Positives', 'Cum_Individuals', 'Cum_Number of Deaths']].sum()
 
-        # this fetcher first collected data for 2020-07-21
-        # GBR_PHTW stopped collecting on 2020-06-24
-        # so first we gather some bridging data
-        self.bridging_data()
+        # upsert the data
+        for index, record in df.iterrows():
+            date = record['Date']
+            confirmed = record['Cum_Positives']
+            tested = record['Cum_Individuals']
+            deaths = record['Cum_Number of Deaths']
 
-        # this website doesn't always load properly, so we'll take a few attempts
-        url = 'https://app.powerbi.com/view?r=eyJrIjoiZGYxNjYzNmUtOTlmZS00ODAxLWE1YTEtMjA0NjZhMz'\
-            'lmN2JmIiwidCI6IjljOWEzMGRlLWQ4ZDctNGFhNC05NjAwLTRiZTc2MjVmZjZjNSIsImMiOjh9'
+            success, adm_area_1, adm_area_2, adm_area_3, gid = self.adm_translator.tr(
+                input_adm_area_1='Northern Ireland',
+                input_adm_area_2=None,
+                input_adm_area_3=None,
+                return_original_if_failure=True,
+                suppress_exception=True
+            )
 
-        attempts = 1
-        success = False
-        while attempts < 10:
-            try:
-                self.wd_config()
-                logger.info("Fetching country-level information")
-                self.wd.get(url)
-                self.fetch_national()
-                logger.debug('Fetching regional level information')
-                self.fetch_regional()
-                success = True
-                break
-            except:
-                self.wd.quit()
-                logger.info(f'Failed on attempt {attempts}')
-                attempts = attempts + 1
+            upsert_obj = {
+                'source': self.SOURCE,
+                'date': date,
+                'country': 'United Kingdom',
+                'countrycode': 'GBR',
+                'adm_area_1': adm_area_1,
+                'adm_area_2': None,
+                'adm_area_3': None,
+                'confirmed': confirmed,
+                'dead': deaths,
+                'tested': tested,
+                'gid': gid
+            }
 
-        # on tenth attempt move outside the try-except block to capture error
-        if not success:
-            self.wd_config()
-            logger.info("Fetching country-level information")
-            self.wd.get(url)
-            self.fetch_national()
-            logger.debug('Fetching regional level information')
-            self.fetch_regional()
-
-        self.wd.quit()
+            self.upsert_data(**upsert_obj)
